f8b8ff9bef8c8c7630ee6752030990df93dddcd8
Aditya Repe <116277146+AdityaRepe@users.noreply.github.com>
2024-03-16 15:39:54 +0530

    Add files via upload

---
+++ src/Project_Cancer_Image_Classification.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import os
+import numpy as np
+import time
+from PIL import Image
+import resnet_model as cm
+from tensorflow.keras.preprocessing import image
+import io
+
+st.title("Cancer Image Classification")
+
+st.markdown("\nLung and colon cancer is currently one of the most vital diseases in society, and patients are more likely to be cured if the disease is spotted earlier. Using computer vision for analyzing the lung and colon cancer images will spead up the analysis process.")
+
+uploaded_file = st.file_uploader('Upload An Image')
+
+predict_button = st.button('Predict on uploaded files', on_click=None)
+test_data = st.button('Predict on sample data', on_click=None)
+
+@st.cache
+def create_model():
+    resmodel = cm.create_model()
+    return resmodel
+
+
+def predict(image_1,resmodel,predict_button = predict_button):
+    start = time.process_time()
+    if predict_button:
+        if (image_1 is not None):
+            start = time.process_time()  
+            # image_1 = Image.open(image_1).convert("RGB") #converting to 3 channels
+            # image_1 = np.array(image_1)/255
+            # st.write(image_1)
+            # img = image.load_img(image_1, target_size=(224,224))
+            x = image.img_to_array(image_1)
+            st.image([image_1],width=300)
+
+            caption = cm.predict_Category(x,resmodel)
+            st.markdown(" ### **Class:**")
+            impression = st.empty()
+            impression.write(caption)
+            time_taken = "Time Taken for prediction: %i seconds"%(time.process_time()-start)
+            st.write(time_taken)
+            del image_1
+        else:
+            st.markdown("## Upload an Image")
+
+def predict_sample(resmodel,folder = './test_images'):
+    no_files = len(os.listdir(folder))
+    file = np.random.randint(1,no_files)
+    file_path = os.path.join(folder,str(file))
+    image_1 = os.path.join(file_path,os.listdir(file_path)[0])
+    st.write(image_1)
+    img = image.load_img(image_1, target_size=(224,224))
+    predict(img,resmodel, True)
+
+def predict_uploadded(image_1, resmodel):
+    if image_1 is not None:
+        size = 224,224
+        img = Image.open(image_1)
+        img = img.convert('RGB')
+        img = img.resize(size, Image.NEAREST)
+        predict(img,resmodel, True)
+    
+resmodel = create_model()
+
+
+if test_data:
+    predict_sample(resmodel)
+else:
+    predict_uploadded(uploaded_file,resmodel)
---
+++ src/resnet_model.py
@@ -0,0 +1,50 @@
+import joblib
+import os
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.applications import ResNet101
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet import preprocess_input
+
+
+def create_model():
+    """
+    creates the best model ie the attention model
+    and returns the model after loading the weights
+    """
+    Image_Size=[224,224]
+
+    resnet=Sequential()
+
+    resnet.add(ResNet101(input_shape=Image_Size+[3],weights='imagenet',include_top=False,pooling='average'))
+
+    # This is to ensure the base won't be trained again
+    for layer in resnet.layers:
+        layer.trainable=False
+        
+    flatten=Flatten()(resnet.output)
+    dense=Dense(256,activation='relu')(flatten)
+    dense=Dense(128,activation='relu')(dense)
+    dense=Dropout(0.2)(dense)   # We add a dropout here to prevent overfitting
+    prediction=Dense(5,activation='softmax')(dense)
+
+    resmodel=Model(inputs=resnet.input,outputs=prediction)
+    resmodel.load_weights('./models/resnet101_weights.h5')
+    return resmodel
+
+def predict_Category(image1, model):
+    """given image1 filepaths returns the predicted caption
+    """
+
+    # img = image.load_img(img_path, target_size=(224,224))
+    # x = image.img_to_array(image1)
+    x = np.expand_dims(image1, axis=0)
+    x = preprocess_input(x)
+    class_names = ['Colon Adenocarcinoma', 'Colon Benign Tissue', 'Lung Adenocarcinoma', 'Lung Benign Tissue', 'Lung Squamous Cell Carcinoma']
+    return class_names[np.argmax(model.predict(x))]
